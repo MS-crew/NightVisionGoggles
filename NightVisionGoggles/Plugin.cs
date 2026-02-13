e7a3b9ad2da5c270f9a0d12d53e240c1b9c53569
Mustafa SAVAŞ <mustafavesavas1@gmail.com>
2026-02-13 13:22:51 +0300

    i forgot this

--- NightVisionGoggles/Plugin.cs
+++ NightVisionGoggles/Plugin.cs
@@ -23,7 +23,7 @@ namespace NightVisionGoggles
 
         public override Version Version { get; } = new Version(1, 3, 0);
 
-        public override Version RequiredExiledVersion { get; } = new Version(9, 10, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(9, 13, 0);
 
         public override void OnEnabled()
         {
